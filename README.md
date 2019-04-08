75e40f8c809b997835ed410bc9e7d9264eadcc10
Shahidul Islam <shahidul2k9@gmail.com>
2019-04-08 22:32:43 +0600

    add source files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ec-forecaster
 Electricity Consumption Forecaster
+https://machinelearningmastery.com/how-to-develop-lstm-models-for-multi-step-time-series-forecasting-of-household-power-consumption/?fbclid=IwAR3Ye9r2TWFfM_qEwMXeZn_Jf9LZ762HdzMvL_x0ULHeuHQzf74EGFh9NZM
+https://machinelearningmastery.com/how-to-develop-lstm-models-for-time-series-forecasting/?fbclid=IwAR28YiZJaYBhq0rMJrHJS_KRdv_Mi19egSR0u-lEmOF-f-cXAMkuAe0ITeA
+https://www.shanelynn.ie/using-pandas-dataframe-creating-editing-viewing-data-in-python/
+
+1827 ==> 1288 + 539 ==> 184*7 + 77*7 ==> 261*7
+
+
---
+++ src/bd_power_data_preparator.py
@@ -0,0 +1,43 @@
+from numpy import nan
+from numpy import isnan
+from pandas import read_csv
+from pandas import to_numeric
+
+# fill missing values with a value at the same time one day ago
+def fill_missing(values):
+	one_day = 2 * 24
+	for row in range(values.shape[0]):
+		for col in range(1):
+			v = values[row, col]
+			if isnan(v) or v <= 1:
+				values[row, col] = values[row - one_day, col]
+
+# load all data
+dataset = read_csv('../resources/electricity_consumption_orginal.csv', sep=',', header=0, low_memory=False, infer_datetime_format=True, parse_dates={'datetime':[0,1]}, index_col=['datetime'])
+# mark all missing values
+dataset.replace('?', nan, inplace=True)
+# make dataset numeric
+dataset = dataset.astype('float32')
+
+#Need to delete 24:00:00 datas
+dataset.to_csv('../resources/electricity_consumption_30min.csv')
+
+
+
+# resample minute data to total for each 30 minutes
+dataset = read_csv('../resources/electricity_consumption_30min.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
+thirty_minute_groups = dataset.resample('30min')
+thirty_minute_data = thirty_minute_groups.sum()
+fill_missing(thirty_minute_data.values)
+print(thirty_minute_data.shape)
+print(thirty_minute_data.head())
+thirty_minute_data.to_csv('../resources/electricity_consumption_30min.csv')
+
+
+# resample minute data to total for each day
+dataset = read_csv('../resources/electricity_consumption_30min.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
+daily_minute_groups = dataset.resample('D')
+daily_minute_data = daily_minute_groups.sum()
+print(daily_minute_data.shape)
+print(daily_minute_data.head())
+daily_minute_data.to_csv('../resources/electricity_consumption_daily.csv')
